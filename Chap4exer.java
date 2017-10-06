5bd7209ea41963bdfed032d0f5db8076b6921303
Alhamdu U Bello <ab570@sussex.ac.uk>
2017-10-06 20:46:21 +0100

    Javadev version upload

---
+++ Chap4exer.java
@@ -0,0 +1,8 @@
+public class Chap4exer
+ {
+   public static void main(String[] args)
+    {
+      String name = "Alhamdu";
+      System.out.println("Hello, "+ name + "!!!");
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables
+ {
+   public static void main(String[] args)
+    {
+      int number = 100;
+      double rate = 2.314;
+      String firstName;
+      char letter;
+      boolean flag;
+      
+      int salary, hoursWorked;
+      String middleName, lastName, initials;
+
+      firstName = "Ann"; //Assignment Statements
+      number = 25 * 5; // This is how to apply comments
+      letter = 'a';
+      flag = true;
+
+    }
+}
